b40f17b4daac655ec3b212acb2345d310d2eb797
Sho Kusano <sho-kusano@cookpad.com>
2012-12-09 13:01:01 +0900

    $ rails new rails_with_websocket

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWithWebsocket::Application.config.secret_token = 'b8a8c25f5371b60d463a2ff5794d6c51c0c422f5321b6745a19f8125e2e1edc6dd16c1043d8c8bb9060eec9fa123308ceb3a9af9e0b2be9b3c649dc1d6961216'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsWithWebsocket::Application.config.session_store :cookie_store, key: '_rails_with_websocket_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsWithWebsocket::Application.config.session_store :active_record_store
